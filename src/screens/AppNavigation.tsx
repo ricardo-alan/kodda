6a59b952835ce2c05ba6af1861fdff63ef575391
Rick Hernandez <mrrahg@gmail.com>
2023-01-21 22:21:43 -0600

    feat: navigation added and initial libs

---
+++ src/screens/AppNavigation.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {UsersSecreen} from './';
+
+const Stack = createNativeStackNavigator();
+
+export const AppNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Users" component={UsersSecreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
